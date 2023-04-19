9399993cf442668b265feda767ab397a8057484f
YeisonMauricio28004 <116232628+YeisonMauricio28004@users.noreply.github.com>
2023-04-18 20:18:23 -0600

    Codigo de la Imagen ya con el mensaje incluido y terminado

---
+++ RespuestaDelCodigo/RespuestaDelCodigo/Program.cs
@@ -0,0 +1,12 @@
+﻿int dato = 16;
+String dato2 = "25g";
+
+try
+{
+    int resultado = dato + Convert.ToInt32(dato2);
+
+}
+catch (Exception)
+{
+    Console.WriteLine("Lo sentimos no se pudo realizar la operación solicitada");
+}
